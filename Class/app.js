22f707d5b52afd97a0cb76fa825c05ccda00ef54
Zia Uddin <ziamaryam2526@gmail.com>
2025-10-03 18:40:31 +0500

    Variables in JavaScript

---
+++ Class/app.js
@@ -0,0 +1,43 @@
+// var firstname = "Asim"
+// var lastname = "Khan"
+// lastname = "Raza"
+// console.log(firstname + " " + lastname)
+
+
+// var studentname = "Asim"
+// var studentage = 20
+// var studentclass = "B.Tech"
+// console.log("Student Name is " + studentname);
+// console.log("Student Age is:" + studentage);
+// console.log("Student Class is " + studentclass);
+
+
+// Question 1
+var fruit = "Apple"
+console.log(fruit);
+
+// Question 2
+var city = "Karachi"
+city = "Lahore"
+console.log(city);
+
+// Question 3
+var marks = 50
+marks = 50 + 10
+console.log(marks);
+
+// Question 4
+var temp = ""
+console.log(temp);
+temp = 30 + "C"
+console.log(temp);
+
+// Question 5
+var name = "Ali" 
+var age = 18
+var hobby = "Football"
+console.log("My name is" + " " + name +  " "+"I am " 
++ age  + " " +"years old" +  " " +"I like " + hobby);
+
+
+document.write("Hello World")
