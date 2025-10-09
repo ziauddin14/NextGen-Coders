ae89001a01c3ce44485c5e18efba8a2862836133
Zia Uddin <ziamaryam2526@gmail.com>
2025-10-09 06:33:04 -0700

    Update app.js

--- Class/app.js
+++ Class/app.js
@@ -13,31 +13,66 @@
 
 
 // Question 1
-var fruit = "Apple"
-console.log(fruit);
+// var fruit = "Apple"
+// console.log(fruit);
 
-// Question 2
-var city = "Karachi"
-city = "Lahore"
-console.log(city);
+// // Question 2
+// var city = "Karachi"
+// city = "Lahore"
+// console.log(city);
 
-// Question 3
-var marks = 50
-marks = 50 + 10
-console.log(marks);
+// // Question 3
+// var marks = 50
+// marks = 50 + 10
+// console.log(marks);
 
-// Question 4
-var temp = ""
-console.log(temp);
-temp = 30 + "C"
-console.log(temp);
+// // Question 4
+// var temp = ""
+// console.log(temp);
+// temp = 30 + "C"
+// console.log(temp);
 
-// Question 5
-var name = "Ali" 
-var age = 18
-var hobby = "Football"
-console.log("My name is" + " " + name +  " "+"I am " 
-+ age  + " " +"years old" +  " " +"I like " + hobby);
+// // Question 5
+// var name = "Ali" 
+// var age = 18
+// var hobby = "Football"
+// console.log("My name is" + " " + name +  " "+"I am " 
+// + age  + " " +"years old" +  " " +"I like " + hobby);
 
+// var myName = "Zia Uddin"
+// alert(myName)
 
-document.write("Hello World")
+// var age = 15
+// alert("Famia's age is: " + age)
+//  var myname = "Famia"
+//  alert(myname)
+
+// var fisrt_City = "Karachi"
+// alert(fisrt_City)
+// var _fisrtcity = "lahore"
+// console.log(_fisrtcity);
+
+//JavaScript Identifiers
+// Variables are identified with unique names called identifiers.
+
+// Names can be short like x, y, z.
+
+// Names can be descriptive age, sum, carName.
+
+// The rules for constructing names (identifiers) are:
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter, a $ sign or an underscore (_).
+// var _fisrtcity = "lahore"
+// console.log(_fisrtcity);
+// Names are case sensitive (X is different from x).
+// var name = "Uma"
+// var Name = "ima"
+// Reserved words (JavaScript keywords) cannot be used as names.
+// var,let,contain, for.
+
+var num = 50 - 48
+num = 50*2
+num = 50/10
+num = 50%3
+console.log(num);
